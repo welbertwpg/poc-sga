4d6951f7ce9d50c76e66f06027ae7bbe82df2874
Welbert Gomides <welbert.gomides@gmail.com>
2019-07-28 21:03:29 -0300

    Conexao com fila

---
+++ src/Monitoramento/MonitoramentoApi/AMQP/ConexaoFila.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MonitoramentoApi.AMQP
+{
+    public class ConexaoFila : IDisposable
+    {
+        private readonly IConnection conexao;
+        private readonly IModel canal;
+
+        public ConexaoFila(string host, string fila)
+        {
+            var factory = new ConnectionFactory() { HostName = host };
+            conexao = factory.CreateConnection();
+            canal = conexao.CreateModel();
+
+            canal.QueueDeclare(queue: fila,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
+
+        public void EnviarMensagem(object mensagem)
+            => EnviarMensagem(JsonConvert.SerializeObject(mensagem));
+
+        public void EnviarMensagem(string mensagem)
+        {
+            var properties = canal.CreateBasicProperties();
+            properties.Persistent = true;
+
+            canal.BasicPublish(exchange: "",
+                                routingKey: "task_queue",
+                                basicProperties: properties,
+                                body: Encoding.UTF8.GetBytes(mensagem));
+        }
+
+        public void Dispose()
+        {
+            if (canal != null)
+                canal.Dispose();
+
+            if (conexao != null)
+                conexao.Dispose();
+        }
+    }
+}
