38e818c70835668f2b7afc30584e100b8ab92ce8
Welbert Gomides <welbert.gomides@gmail.com>
2019-08-15 16:31:10 -0300

    Testes da validacao de cronograma

---
+++ src/Ativos/Ativos.Testes/ValidacaoCronograma.cs
@@ -0,0 +1,81 @@
+﻿using Ativos.Dominio.Entidades;
+using Ativos.Dominio.Validacoes;
+using FluentValidation;
+using Xunit;
+
+namespace Ativos.Testes
+{
+    public class ValidacaoCronograma
+    {
+        private readonly IValidator<CronogramaManutencao> validadorCronograma;
+
+        public ValidacaoCronograma()
+            => validadorCronograma = new ValidadorCronograma();
+
+        [Fact]
+        public void Validacao_CamposObrigatoriosPreenchidos_Valido()
+        {
+            var cronograma = new CronogramaManutencao
+            {
+                Frequencia = FrequenciaManutencao.Diaria,
+                TipoAtivo = TipoAtivo.Equipamento
+            };
+
+            var resultadoValidacao = validadorCronograma.Validate(cronograma);
+            Assert.True(resultadoValidacao.IsValid);
+        }
+
+        [Fact]
+        public void Validacao_CamposObrigatoriosNaoPreenchidos_InvalidoComErros()
+        {
+            var cronograma = new CronogramaManutencao { };
+            var resultadoValidacao = validadorCronograma.Validate(cronograma);
+            Assert.False(resultadoValidacao.IsValid);
+            Assert.Equal(2, resultadoValidacao.Errors.Count);
+        }
+
+        [Fact]
+        public void Validacao_IntervaloHorasUsoFrequenciaValida_Valido()
+        {
+            var cronograma = new CronogramaManutencao
+            {
+                Frequencia = FrequenciaManutencao.Intervalo,
+                IntervaloHorasUso = 2,
+                TipoAtivo = TipoAtivo.Equipamento
+            };
+
+            var resultadoValidacao = validadorCronograma.Validate(cronograma);
+            Assert.True(resultadoValidacao.IsValid);
+        }
+
+        [Fact]
+        public void Validacao_IntervaloHorasUsoFrequenciaInvalida_Invalido()
+        {
+            var cronograma = new CronogramaManutencao
+            {
+                Frequencia = FrequenciaManutencao.Diaria,
+                IntervaloHorasUso = 2,
+                TipoAtivo = TipoAtivo.Equipamento
+            };
+
+            var resultadoValidacao = validadorCronograma.Validate(cronograma);
+            Assert.False(resultadoValidacao.IsValid);
+            Assert.Equal(1, resultadoValidacao.Errors.Count);
+        }
+
+        [Fact]
+        public void Validacao_IntervaloHorasUsoNegativaFrequenciaValida_Invalido()
+        {
+            var cronograma = new CronogramaManutencao
+            {
+                Frequencia = FrequenciaManutencao.Intervalo,
+                IntervaloHorasUso = -2,
+                TipoAtivo = TipoAtivo.Equipamento
+            };
+
+            var resultadoValidacao = validadorCronograma.Validate(cronograma);
+            Assert.False(resultadoValidacao.IsValid);
+            Assert.Equal(1, resultadoValidacao.Errors.Count);
+        }
+    }
+}
