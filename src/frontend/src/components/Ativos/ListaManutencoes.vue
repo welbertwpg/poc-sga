03880d1792fb68cbe31801749801e71cf082ba31
Welbert Gomides <welbert.gomides@gmail.com>
2019-08-18 18:16:41 -0300

    Mensagem de nenhuma manutenção

--- src/frontend/src/components/Ativos/ListaManutencoes.vue
+++ src/frontend/src/components/Ativos/ListaManutencoes.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <md-list>
+      <md-list-item  v-if="!ativo.manutencoes.length">
+        <span>Nenhuma manutenção para este ativo</span>
+      </md-list-item>
+
       <md-list-item v-for="manutencao in ativo.manutencoes" v-bind:key="manutencao.identificador">
-        
         <div class="md-list-item-text">
           <span>{{ manutencao.dataHora }}</span>
           <span>{{ manutencao.tipo }}</span>
         </div>
-        
+
         <md-icon v-if="manutencao.realizada" class="md-primary">check</md-icon>
 
         <md-button class="md-icon-button md-list-action" v-if="!manutencao.realizada">
