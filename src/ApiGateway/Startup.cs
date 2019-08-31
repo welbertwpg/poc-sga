34bff92eb4f881d2d13ff8b6106913d069ddc56d
Welbert Gomides <welbert.gomides@gmail.com>
2019-08-31 15:22:17 -0300

    Alteração na estrutura e suporte a docker

---
+++ src/ApiGateway/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowCredentials());
+            });
+
+            services.AddOcelot(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseWebSockets();
+
+            await app.UseOcelot();
+        }
+    }
+}
---
+++ src/Monitoramento/Monitoramento.Atualizador/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using Monitoramento.Dominio.Interfaces;
+using Monitoramento.Infra.Servicos;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Threading;
+
+namespace Monitoramento.Atualizador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var endpointSignalr = configuration["Signalr.Endpoint"];
+            var conexao = new HubConnectionBuilder()
+                .WithUrl(endpointSignalr)
+                .Build();
+
+            conexao.Closed += async (error) =>
+            {
+                logger.Information("Conexão encerrada, reconectando");
+                await conexao.StartAsync();
+            };
+
+            logger.Information($"Conectando no endereço {endpointSignalr}");
+            conexao.StartAsync().Wait();
+            logger.Information("Conectado");
+
+            var intervalo = Convert.ToInt32(configuration["Intervalo"]);
+            logger.Information($"Tempo de atualização: {intervalo}ms");
+
+            ISensores sensores = new MockSensores();
+            while (true)
+            {
+                var resultado = sensores.Obter();
+                var resultadoJson = JsonConvert.SerializeObject(resultado);
+                logger.Information($"Novo resultado: {resultadoJson}");
+
+                conexao.InvokeAsync("AtualizarResultadosSensores", resultado).Wait();
+                logger.Information("Enviado para o SignalR");
+
+                Thread.Sleep(intervalo);
+            }
+        }
+    }
+}
